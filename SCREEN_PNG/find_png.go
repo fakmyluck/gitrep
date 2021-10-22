24b05300200e61e85e4a2b93a698c8121acd7f9d
Slucker <akozuhovski@gmail.com>
2021-10-23 00:31:08 +0300

    fpic

--- SCREEN_PNG/find_png.go
+++ SCREEN_PNG/find_png.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"image/png"
 	_ "image/png"
 	"os"
+	"slucker/SCREEN_PNG/screenshot"
 )
 
 type picture struct {
@@ -15,15 +17,29 @@ type picture struct {
 	sym string
 }
 
+type time struct {
+	day   int
+	mon   int
+	year  int
+	hour  int
+	minut int
+	sec   int
+}
+
 type dimensions struct {
 	Dx, Dy int //dlinna shirina
 }
 
 func main() {
 
+	////
+
+	/////
 	var num [12]picture
 	var control [4]picture
-	Screenshot, maxY, maxX := createpic("Screenshot", "tstsmple"), 0, 0
+	Screenshot := takescreenshot("Screenshot")
+	//Screenshot  := createpic("Screenshot", "tstsmple")
+	maxY, maxX := 0, 0
 	sDy, sDx := Screenshot.dim.Dy, Screenshot.dim.Dx
 	//var tX,tY int
 	control[0] = createpic("HZ", "HZ")
@@ -55,21 +71,21 @@ func main() {
 
 			if Screenshot.searchPic(x, y, control[c]) { //zapisat' "word" esli naidena cifra
 				switch c {
-				case 0:
+				case 0: //hz
 					{
-						hz = Screenshot.findvert(x, y+12, num)
-						y = y + 12
-						x = x + 40
+						hz = Screenshot.findvert(x, y+14, num)
+						y = y + 14
+						x = x + 100
 					}
-				case 1:
+				case 1: //andre
 					andres = true
 					y = y + 256
 
-				case 2:
-					start = Screenshot.findvert(x+control[2].dim.Dx+30, y-3, num)
+				case 2: //start
+					start = Screenshot.findvert(x+control[2].dim.Dx+25, y-3, num)
 
-				case 3:
-					end = Screenshot.findvert(x+control[3].dim.Dx+40, y-4, num)
+				case 3: //end
+					end = Screenshot.findvert(x+control[3].dim.Dx+38, y-4, num)
 
 				case 4:
 					fmt.Println("ERROR case4!")
@@ -86,22 +102,76 @@ func main() {
 
 printresults:
 
+	//FUNC podshet
+	startInt := toTime(start)
+	endtInt := toTime(end)
+
 	fmt.Printf("Hz: %v\n", hz)
 	fmt.Printf("andres: %v\n", andres)
 	fmt.Printf("Start: %v\n", start)
 	fmt.Printf("End: %v\n", end)
 
-	//fmt.Println(word)
+	fmt.Printf("\nStart: %v\n", startInt)
+	fmt.Printf("End: %v\n", endtInt)
+	// fmt.Printf("myImg type: %T\n", Screenshot.img)
+	// fmt.Println(word)
 
-	// outputFile, err := os.Create("obvedenniy_3.png")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	outputFile, err := os.Create("obvedenniy_3.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// png.Encode(outputFile, Screenshot.img)
-	// fmt.Println("File created.")
+	png.Encode(outputFile, Screenshot.img)
+	fmt.Println("File created.")
 
+}
+
+func toTime(s string) time {
+	T := time{
+		sec:   loopstring(&s),
+		minut: loopstring(&s),
+		hour:  loopstring(&s),
+		year:  loopstring(&s),
+		mon:   loopstring(&s),
+		day:   loopstring(&s),
+	}
+	return T
+}
+
+func loopstring(s *string) int {
+
+	var sum int
+	//fmt.Println(len((*s)))
+	i := len(*s) - 1
+
+	mult := 1
+	for ; i >= 0 && (*s)[i] != ' ' && (*s)[i] != ':' && (*s)[i] != '.'; i-- {
+		sum += (int((*s)[i]) - '0') * mult
+		mult *= 10
+	}
+	if i >= 0 {
+		*s = (*s)[:i]
+	}
+
+	return sum
+}
+
+func takescreenshot(symbol string) picture {
+
+	var char picture
+	char.sym = symbol
+
+	var err error
+	char.ptr, err = screenshot.CaptureScreen()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	char.img = image.Image(char.ptr)
+
+	char.dim.Dy, char.dim.Dx = char.ptr.Bounds().Dy(), char.ptr.Bounds().Dx()
+	return char
 }
 
 func createpic(symbol, filename string) picture {
@@ -124,36 +194,27 @@ func createpic(symbol, filename string) picture {
 	return char
 }
 
-func (bigPic picture) findvert(x, y int, num [12]picture) string {
+func (bigPic *picture) findvert(x, y int, num [12]picture) string {
 	x0 := x
 	var word string
+	var tmp int
+
+	bigPic.redcross(x, y)
 
 	for xt := x; xt+6 < bigPic.dim.Dx && xt-x0 < 10; xt++ {
 		for yt := y; yt < y+10; yt++ {
 
 			for n := 0; n < 12; n++ {
 				if bigPic.searchPic(xt, yt, num[n]) {
+
+					if xt-x0-num[tmp].dim.Dx > 2 {
+						word += " "
+					}
+					bigPic.greenbox(xt, yt, num[n])
 					word += num[n].sym
+
 					x0 = xt
-				}
-			}
-
-		}
-	}
-	return word
-}
-
-func (bigPic picture) findhor(x, y int, num [12]picture) string {
-	x0 := x
-	var word string
-
-	for yt := y; yt < y+11; yt++ {
-		for xt := x; xt+6 < bigPic.dim.Dx && xt-x0 < 8; xt++ {
-
-			for n := 0; n < 12; n++ {
-				if bigPic.searchPic(xt, yt, num[n]) {
-					word += num[n].sym
-					x0 = xt
+					tmp = n
 				}
 			}
 
@@ -177,6 +238,14 @@ func (bigPic *picture) searchPic(x, y int, obj picture) bool {
 	}
 	bigPic.greenbox(x, y, obj)
 	return true
+}
+
+func (screenshot *picture) redcross(x, y int) {
+	screenshot.ptr.SetRGBA(x, y, color.RGBA{255, 0, 0, 255}) //RED +
+	screenshot.ptr.SetRGBA(x-1, y, color.RGBA{255, 0, 0, 255})
+	screenshot.ptr.SetRGBA(x+1, y, color.RGBA{255, 0, 0, 255})
+	screenshot.ptr.SetRGBA(x, y-1, color.RGBA{255, 0, 0, 255})
+	screenshot.ptr.SetRGBA(x, y+1, color.RGBA{255, 0, 0, 255})
 }
 
 func (screenshot *picture) greenbox(x, y int, num picture) {
